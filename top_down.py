7312ad81cb22f8932ed40c93382d288414b20f0b
KevinEsh <kevin_esh@outlook.com>
2020-02-19 15:47:54 -0600

    La funcion is_intersected funciona a la perfeccion

--- top_down.py
+++ top_down.py
@@ -7,22 +7,30 @@ class Tank:
         self.name = name
         self.capacity = max_cap
         self.lazy = lazy
-        self.cheve = None
-        self.start = -float("inf")
-        self.end = float("inf")
+        self.beer = None
+        self.interval = [ (-float("inf"), float("inf")) ]
 
 
 class Query:
-    def __init__(self, cheve, start, end, amount):
-        self.cheve = cheve
+    def __init__(self, beer, start, end, amount):
+        self.beer = beer
         self.start = start
         self.end = end
         self.amount = amount
 
+def is_intersected(interval1, interval2):
+    return False if interval2[0] > interval1[1] or interval1[0] > interval2[1] else True
 
 def top_down(orders , tanks):
     sorted_orders = sorted(orders, key =  lambda orden : orden.start, reverse=True)
-    print([(x.cheve, x.start, x.end) for x in sorted_orders] )
+    print( [(x.cheve, x.start, x.end) for x in sorted_orders] )
+
+    for order in orders:
+        
+        feasible_tanks = [tank for tank in tanks if tank.beer == order.beer and tanks.capacity != 0]
+        feasible_tanks += [tank for tank in tanks if tank == None and not is_intersected( tanks.interval, [order.start, order.end] ) ]
+        
+
     return    
 
 
@@ -38,13 +46,16 @@ if __name__ == "__main__":
             query = file.readline().split(' ')
             query = Query( query[0], int(query[1]), int(query[2]) , int(query[3]))
             queries.append(query)
-            print(query.cheve, query.start, query.end, query.amount)
+            print(query.beer, query.start, query.end, query.amount)
 
         for i in range(n_tanks):
             name, capacity = file.readline().split(' ')
             tank = Tank(name, int(capacity), 30)
             tanks.append(tank)
-            print(tank.name, tank.capacity, tank.cheve, tank.start, tank.end)
+            print(tank.name, tank.capacity, tank.beer, tank.interval)
 
-    top_down(queries, tanks)    
-    
+    for query1 in queries:
+        for query2 in queries:
+            print( (query1.start, query1.end), (query2.start, query2.end))
+            print(is_intersected([query1.start, query1.end], [query2.start, query2.end]) )
+    #top_down(queries, tanks)
